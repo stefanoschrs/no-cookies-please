26d7adb067385d065d505d7bfd4d530a3325cd9c
Stefanos Chrs <root@stefanoschrs.com>
2020-05-28 12:06:33 +0200

    - Add Piwik.pro - Add 1touch.io

--- extension/background.js
+++ extension/background.js
@@ -15,11 +15,13 @@ function onTabUpdate(tabId, changeInfo, tab) {
     for (let i = 0; i < 15; i++) {
       setTimeout(removeCybot, i * 150)
       setTimeout(removeOnetrust, i * 150)
+      setTimeout(removePiwik, i * 150)
+      setTimeout(remove1touch, i * 150)
     }
 
     // https://www.cybot.com
     function removeCybot () {
-      let el = document.querySelector('#CybotCookiebotDialog')
+      let el = document.querySelector('[id="CybotCookiebotDialog"]')
       if (el && el.style.display !== 'none') {
         el.style.display = 'none'
         chrome.runtime.sendMessage({
@@ -35,7 +37,7 @@ function onTabUpdate(tabId, changeInfo, tab) {
 
     // https://www.onetrust.com/
     function removeOnetrust () {
-      let el = document.querySelector('#onetrust-consent-sdk')
+      let el = document.querySelector('[id="onetrust-consent-sdk"]')
       if (el && el.style.display !== 'none') {
         el.style.display = 'none'
         chrome.runtime.sendMessage({
@@ -44,6 +46,38 @@ function onTabUpdate(tabId, changeInfo, tab) {
           meta: {
             tabId: tabId,
             name: 'onetrust'
+          }
+        })
+      }
+    }
+
+    // https://piwik.pro/
+    function removePiwik () {
+      let el = document.querySelector('div[id^="ppms_cm_consent_"]')
+      if (el && el.style.display !== 'none') {
+        el.style.display = 'none'
+        chrome.runtime.sendMessage({
+          app: appName,
+          path: pathFound,
+          meta: {
+            tabId: tabId,
+            name: 'piwik'
+          }
+        })
+      }
+    }
+
+    // https://1touch.io/
+    function remove1touch () {
+      let el = document.querySelector('[id="hs-eu-cookie-confirmation"]')
+      if (el && el.style.display !== 'none') {
+        el.style.display = 'none'
+        chrome.runtime.sendMessage({
+          app: appName,
+          path: pathFound,
+          meta: {
+            tabId: tabId,
+            name: '1touch'
           }
         })
       }
